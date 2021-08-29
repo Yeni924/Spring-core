510b823f1514f71e8620f54eb96269afad907880
kwonyeeun <ktech960924@gmail.com>
2021-08-29 19:24:24 +0900

    싱글톤 패턴 적용 및 확인 - 싱글톤 패턴은 새로운 객체를 생성하지 않고 한개의 객체를 공유해서 씀

---
+++ src/test/java/singleton/SingletonService.java
@@ -0,0 +1,23 @@
+package singleton;
+
+public class SingletonService {
+
+    //static 영역에 객체 instance를 하나 생성해서 올린다.
+    private static final SingletonService instance = new SingletonService();
+
+    // getInstance()메서드를 통해서만 조회할 수 있다.
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+
+    //딱 1개의 객체 인스턴스만 존재야하 하므로, 생성자를 private로 만들어 외부에서 생성되는것을 막는다.
+    private SingletonService(){
+        
+    }
+    
+    public  void logit(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+    
+}
--- src/test/java/singleton/SingletonTest.java
+++ src/test/java/singleton/SingletonTest.java
@@ -29,4 +29,16 @@ public class SingletonTest {
         //memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
