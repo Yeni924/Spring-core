0ae55cbfa4d8cfae5862480bab980c9e270619c5
kwonyeeun <ktech960924@gmail.com>
2021-08-31 19:26:50 +0900

    Test : 추가,  @ComponentScan AutoConfig 설정 정보에서 자동 빈 생성, 주입 되는지 확인
     - AutoConfig 설정 정보에서 자동 빈 생성, 주입 되는지 확인하는 테스트
     - MemeberService에 memberServiceImpl,memoryMemberRepositor가 잘 주입됌

---
+++ src/test/java/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package scan;
+
+import com.example.core.AutoAppConfig;
+import com.example.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
