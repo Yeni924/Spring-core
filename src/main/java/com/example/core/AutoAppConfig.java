da2c4274e9392e5805de6dc2dfe4c4d190ced8c1
kwonyeeun <ktech960924@gmail.com>
2021-08-31 20:48:16 +0900

    Comment : "컴포넌트 스캔 빈 중복 충돌", 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생한다.
    - AutoAppConfig 이미 등록되있는 memoryMemberRepository를 재등록 후 테스트 돌리면, 오류가 나지 않는다.
    - 수동 등록한 빈이 우선권을 가져 실행된다.
    - but, 스프링 부트는 수동 빈 등록과 자동 빈 등록이 충돌이 되면 오류가 발생하도록 기본값으로 설정했다.
    
    프로젝트 시작루트인 com.example.core에 설정 정보를 두고 basePackages는 생략한다.

--- src/main/java/com/example/core/AutoAppConfig.java
+++ src/main/java/com/example/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package com.example.core;
 
+import com.example.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +14,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+// 빈 중복 예제 관련 테스트이나, 부트 실행시 오류 발생하므로 주석 처리
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemoryMemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
 }
