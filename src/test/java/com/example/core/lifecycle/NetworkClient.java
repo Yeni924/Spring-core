26df6c9c7c47068e31d44a302aa89078bc0a4d62
kwonyeeun <ktech960924@gmail.com>
2021-09-02 20:11:06 +0900

    "Bean(initMethod = "init", destroyMethod = "close")"
    [장점]
    - 스프링 빈이나 스프링 코드에 의존적이지 않다.
    - 메서드 이름을 자유롭게 줄 수 있다.
    - 설정 정보를 사용 해 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.

--- src/test/java/com/example/core/lifecycle/NetworkClient.java
+++ src/test/java/com/example/core/lifecycle/NetworkClient.java
@@ -1,9 +1,8 @@
 package com.example.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+
+public class NetworkClient {
 
     private String url;
 
@@ -28,15 +27,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : "+url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
+
 }
