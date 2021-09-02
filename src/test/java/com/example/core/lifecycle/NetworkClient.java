2e5641648028ff0190e8afd2ff73bcc451b82023
kwonyeeun <ktech960924@gmail.com>
2021-09-02 20:16:47 +0900

    Comment: "@PostConstruct, @PreDestory 초기화 시점을 알려주는 다양한 방법
    @PostConstruct : 초기화시점
    @PreDestory : 소멸 시점

--- src/test/java/com/example/core/lifecycle/NetworkClient.java
+++ src/test/java/com/example/core/lifecycle/NetworkClient.java
@@ -1,6 +1,8 @@
 package com.example.core.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class NetworkClient {
 
@@ -27,12 +29,14 @@ public class NetworkClient {
         System.out.println("close : "+url);
     }
 
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
