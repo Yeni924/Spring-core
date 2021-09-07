d17974cca5ebd95109c52eed0345f73806210c1b
kwonyeeun <ktech960924@gmail.com>
2021-09-07 19:40:52 +0900

    Comment: 프로토타입 스코프 + 싱글톤 빈 Provider로 문제 해결
    -ObjectFactory,ObjectProvider
    -JSR-330 Provider

--- src/test/java/com/example/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/example/core/scope/SingletonWithPrototypeTest1.java
@@ -2,11 +2,14 @@ package com.example.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Provider;
+
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -36,24 +39,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
-
 
         @Autowired
-       public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
+
 
         public int logic(){
+            PrototypeBean prototypeBean =   prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
+
 
     @Scope("prototype")
     static class PrototypeBean{
